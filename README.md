3dd5fad3ca75e2fcacdd417164c3fb2017f2aea0
leon gongola <leongongola@gmai.com>
2022-08-25 13:37:45 -0600

    commit

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 Hie, welcome to my weather app. I started coding the web app during my #100days of code.  
 
-This app show the current weather and a 7 day weather forecast fora particular city.
-I used the open weather API to get my data for the current weather 
+This app show the current weather forecast for a particular city.
+I used the open weather API to get my data for the current weather.
+
+Used the 3D weather icons from uifreebies.net, to show the weather icon according to the weather data for that particular date.
---
+++ script.js
@@ -0,0 +1,25 @@
+let weather ={
+    apiKey: "84ff045ff659866878158ecff63b7963",
+    fetchWeather: function (city) {
+     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey)
+     .then((response) => response.json())
+     .then((data) => this.displayWeather(data)); 
+    },
+    
+    displayWeather: function(data){
+       const {name} = data;
+       const {icon, description} = data.weather[0];
+       const {temp, humidity} = data.main;
+       const {speed} = data.wind;
+
+       console.log(name, icon, temp, humidity, speed, description);
+
+       document.getElementById("city-name").innerText = name;
+       document.getElementById("weather-icon").src = "/weather icons/cloud/35.png";
+       document.getElementById("type-of-weather").innerText = description;
+       document.getElementById("wind").innerText = `${speed} km/h`;
+       document.getElementById("temperature").innerText = `${Math.floor(temp)}°C`;
+       document.getElementById("humidity").innerText = `${humidity}%`
+
+    }
+}
