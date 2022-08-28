fdcd866094e515088657da6ad9ddf37b9c340776
leon gongola <leongongola@gmai.com>
2022-08-27 21:41:23 -0600

    commit

--- js/script.js
+++ js/script.js
@@ -28,59 +28,128 @@ let weather ={
        document.getElementById("fog").innerText = `${visibility/1000}km`;
        
       
-        
-    if ( icon == "01d"){
+    let iconValue = function(){
+        if ( icon === "01d"){
+            "/images/weather icons/Sun 1.png";
+    
+        } else if (icon = "01n") {
+            "/images/weather icons/Moon.png";
+    
+        } else if (icon === "02d") {
+            "/images/weather icons/Sunny.png";
+    
+        } else if (icon === "02n") {
+            "/images/weather icons/Dark Cloud.png";
+    
+        } else if (icon === "03d") {
+            "/images/weather icons/Cloud 6.png";
+    
+        } else if (icon === "03n") {
+            "/images/weather icons/Dark Cloud.png";
+        }
+    
+        else if (icon === "04d") {
+            "/images/weather icons/Cloud 6.png";
+        }
+    
+        else if (icon === "04n") {
+            "/images/weather icons/Dark Cloud.png";
+        }
+    
+        else if (icon === "09d") {
+            "/images/weather icons/Cloudy + Rainy.png";
+        }
+    
+        else if (icon === "09n") {
+            "/images/weather icons/Cloudy + Heavy Rain.png";
+        }
+    
+        else if (icon === "10d") {
+            "/images/weather icons/Sunny + Heavy Rain.png";
+        }
+    
+        else if (icon === "10n") {
+            "/images/weather icons/Dark Cloudy + Heavy Rain.png";
+        }
+    
+        else if (icon === "11d") {
+            "/images/weather icons/Cloudy + Rainy + Lightning.png";
+        }
+    
+        else if (icon === "11n") {
+            "/images/weather icons/Dark Cloudy + Rainy +Lightning.png";
+        }
+    
+        else if (icon === "13d") {
+            "/images/weather icons/Snowflake.png";
+        }
+    
+        else if (icon === "13n") {
+            "/images/weather icons/Snowflake.png";
+        }
+    
+        else if (icon === "50d") {
+            "/images/weather icons/Light Fog.png";
+        }
+    
+        else if (icon === "50n") {
+            "/images/weather icons/Light Fog.png";
+        }else{
+            "/images/weather icons/Sunny.png";
+        }
+    }    
+    if ( icon === "01d"){
         "/images/weather icons/Sun 1.png";
 
-    } else if (icon == "01n") {
+    } else if (icon = "01n") {
         "/images/weather icons/Moon.png";
 
-    } else if (icon == "02d") {
+    } else if (icon === "02d") {
         "/images/weather icons/Sunny.png";
 
-    } else if (icon == "02n") {
+    } else if (icon === "02n") {
         "/images/weather icons/Dark Cloud.png";
 
-    } else if (icon == "03d") {
+    } else if (icon === "03d") {
         "/images/weather icons/Cloud 6.png";
 
-    } else if (icon == "03n") {
+    } else if (icon === "03n") {
         "/images/weather icons/Dark Cloud.png";
     }
 
-    else if (icon == "04d") {
+    else if (icon === "04d") {
         "/images/weather icons/Cloud 6.png";
     }
 
-    else if (icon == "04n") {
+    else if (icon === "04n") {
         "/images/weather icons/Dark Cloud.png";
     }
 
-    else if (icon == "09d") {
+    else if (icon === "09d") {
         "/images/weather icons/Cloudy + Rainy.png";
     }
 
-    else if (icon == "09n") {
+    else if (icon === "09n") {
         "/images/weather icons/Cloudy + Heavy Rain.png";
     }
 
-    else if (icon == "10d") {
+    else if (icon === "10d") {
         "/images/weather icons/Sunny + Heavy Rain.png";
     }
 
-    else if (icon == "10n") {
+    else if (icon === "10n") {
         "/images/weather icons/Dark Cloudy + Heavy Rain.png";
     }
 
-    else if (icon == "11d") {
+    else if (icon === "11d") {
         "/images/weather icons/Cloudy + Rainy + Lightning.png";
     }
 
-    else if (icon == "11n") {
+    else if (icon === "11n") {
         "/images/weather icons/Dark Cloudy + Rainy +Lightning.png";
     }
 
-    else if (icon == "13d") {
+    else if (icon === "13d") {
         "/images/weather icons/Snowflake.png";
     }
 
@@ -94,6 +163,8 @@ let weather ={
 
     else if (icon === "50n") {
         "/images/weather icons/Light Fog.png";
+    }else{
+        "/images/weather icons/Sunny.png";
     }
 
    
